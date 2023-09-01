6c74519bb3f102f0359cf1b6f9d0122d24bf2698
Ana Beatriz Schuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2023-09-01 08:18:08 -0400

    Baixei o router-dom e criei as rotas do sistema

--- sig-mt-protege/src/App.js
+++ sig-mt-protege/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './paginas/login';
 import Cadastro from './paginas/cadastro';
+import PainelPrincipal from './paginas/painel-principal';
+import Policiais from './paginas/crud-policiais';
+import Ocorrencias from './paginas/crud-ocorrencias';
+import Ajuda from './paginas/ajuda';
 
 function App() {
-  return (
-    <Cadastro/>
-  )
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />}/>
+        <Route path="/cadastro" element={<Cadastro />}/>
+        <Route path="/painel-principal" element={<PainelPrincipal />}/>
+        <Route path="/policiais" element={<Policiais />}/>
+        <Route path="/ocorrencias" element={<Ocorrencias />}/>
+        <Route path="/ajuda" element={<Ajuda />}/>
+      </Routes>
+    </BrowserRouter>
+  ); 
 }
 
 export default App;
--- sig-mt-protege/src/paginas/painel-principal.jsx
+++ sig-mt-protege/src/paginas/painel-principal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom';
 
 function PainelPrincipal() {
     return
