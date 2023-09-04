1dcf6ac370350572e767efaeba08027e67f4fc70
Ana Beatriz Schuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2023-09-04 10:17:14 -0400

    Continuação da Estilização do sistema

--- sig-mt-protege/src/paginas/ajuda.jsx
+++ sig-mt-protege/src/paginas/ajuda.jsx
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../imagens/logo-definitiva-mt-protege.png';
 import IconeLogout from '../imagens/icone-logout.png';
 import IconePainelPrincipal from '../imagens/icone-painel-principal.png';
-import IconePolicia from '../imagens/icone-policia.png'
+import IconePolicia from '../imagens/icone-policia.png';
+import IconeOcorrencia from '../imagens/icone-ocorrencia.png';
+import IconeAjuda from '../imagens/icone-ajuda.png';
 import './styles.css';
-import { Link } from 'react-router-dom';
 
 function Ajuda() {
   return (
@@ -18,23 +19,23 @@ function Ajuda() {
           </div>
           <ul className="nav flex-column">
             <li className="nav-item">
-              <a className="nav-link text-light" href="#">
-                <img src={IconePainelPrincipal} alt="Icone Painel Principal" className="icone-painel-principal" />PAINEL PRINCIPAL
+              <a className="nav-link text-light" href="/painel-principal">
+                <img src={IconePainelPrincipal} alt="Icone Painel Principal" className="icones-menu-nav" />PAINEL PRINCIPAL
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-light" href="#">
-              <img src={IconePolicia} alt="Icone Policial" className="icone-policia" />POLICIAIS
+              <a className="nav-link text-light" href="/policiais">
+              <img src={IconePolicia} alt="Icone Policial" className="icones-menu-nav" />POLICIAIS
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-light" href="#">
-                OCORRÊNCIAS
+              <a className="nav-link text-light" href="/ocorrencias">
+                <img src={IconeOcorrencia} alt="Icone Ocorrencia" className="icones-menu-nav" />OCORRÊNCIAS
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-light" href="#">
-                AJUDA / SUPORTE
+              <a className="nav-link text-light" href="/ajuda">
+                <img src={IconeAjuda} alt="Icone Ajuda" className="icones-menu-nav" />AJUDA / SUPORTE
               </a>
             </li>
           </ul>
@@ -43,7 +44,7 @@ function Ajuda() {
          <div style={{ position: 'absolute', bottom: '0', width: '90%' }}>
             <ul className="nav flex-column">
               <li className="nav-item">
-                <a className="nav-link text-light" href="#">
+                <a className="nav-link text-light" href="/">
                   <img src={IconeLogout} alt="Icone Logout" className="icone-logout" />SAIR DO SISTEMA
                 </a>
               </li>
