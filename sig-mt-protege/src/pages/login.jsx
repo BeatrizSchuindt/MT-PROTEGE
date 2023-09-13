58d7680c4f73bd5d88db3af5f56ad8641fe68396
Ana Beatriz Schuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2023-09-13 10:53:11 -0400

    Integrei o Login da API com o Front
    Co-authored-by: MaliaEdualda <MaliaEdualda@users.noreply.github.com>

--- sig-mt-protege/src/pages/login.jsx
+++ sig-mt-protege/src/pages/login.jsx
@@ -1,26 +1,31 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
 import Logo from '../images/logo-definitiva-mt-protege.png';
 import ImgPoliciaEsquerda from '../images/policia-esquerda.jpg';
 import ImgPoliciaDireita from '../images/policia-direita.jpg';
-import './styles.css';
 
-/*
-const Login = () => {
-  const [matricula, setMatricula] = useState('');
-  const [senha, setSenha] = useState('');
-
-  const handleLogin = () => {
-    // Aqui você pode adicionar a lógica de autenticação
-    // Por exemplo, enviar as credenciais para um servidor e verificar se são válidas
-    // Por enquanto, apenas exibiremos as credenciais no console
-    console.log(`Matrícula: ${matricula}, Senha: ${senha}`);
-  };
-
-  no botão onClick={handleLogin}
-*/
+import { loginPolicial } from "../services/policial-services";
 
 function Login() {
+  const { handleSubmit, register, formState: { errors, isValid } } = useForm({ mode: 'onChange' });
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    try {
+      const policial = await loginPolicial(data);
+      navigate('/painel-principal');
+    } catch (error) {
+      setError({message: error.response.data.error})
+    }
+  };
+
   return (
     <div style={{ height: '100vh', backgroundColor: '#00296B', overflow: 'hidden' }}>
       <div className="container-fluid h-100">
@@ -49,17 +54,49 @@ function Login() {
             {/* Quadrado branco para o menu de login abaixo da logo e da frase */}
             <div className="login-box p-3 mt-3 bg-white d-flex flex-column justify-content-center" style={{ width: '60%', height: '45%' }}> {/* Adicionamos a classe bg-white para o fundo branco */}
               <h2 className="text-center">Login</h2>
-              <form>
+              <form className="mb-3" noValidate validated={!errors} onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-3">
-                  <label htmlFor="matricula" className="form-label">Matrícula</label>
-                  <input type="text" className="form-control" id="matricula" placeholder="Digite sua matrícula" />
+                  {error && <p>{error.message}</p>}
+                  <label htmlFor="matricula" className="form-label" style={{ fontWeight: 'Bold' }}>Matrícula</label>
+                  <input className="form-control"
+                    label="Matricula"
+                    type="text"
+                    name="matricula_policial"
+                    placeholder="Insira seu matrícula"
+                    isValid={!errors.matricula_policial}
+                    isInvalid={errors.matricula_policial}
+                    required
+                    {...register('matricula_policial', {
+                      required: {message: 'Matrícula é obrigatória'},
+                      pattern: {
+                        value: /^[0-9]{8}$/,
+                        message: 'Matrícula inválida!'
+                      },
+                      minLength: {value: 8, message: "A matrícula precisa ter no mínimo 8 caracteres."}
+                    })} />
+                  {errors.matricula_policial && <p className="hook-form-error">{errors.matricula_policial.message}</p>}
                 </div>
                 <div className="mb-3">
-                  <label htmlFor="senha" className="form-label">Senha</label>
-                  <input type="password" className="form-control" id="senha" placeholder="Digite sua senha" />
+                  <label htmlFor="senha" className="form-label" style={{ fontWeight: 'Bold' }}>Senha</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="senha"
+                    name="senha"
+                    placeholder="Digite sua senha"
+                    error={errors.senha}
+                    required
+                    {...register('senha', {
+                      required: {message: 'A senha é obrigatória.'},
+                      minLength: {
+                        value: 6,
+                        message: 'A senha precisa ter no mínimo 6 caracteres.'
+                      }
+                    })} />
+                  {errors.senha && <p className="hook-form-error">{errors.senha.message}</p>}
                 </div>
-                <div className="d-flex justify-content-center"> 
-                  <button type="submit" className="btn btn-primary" style={{backgroundColor: '#19A800'}}>ENTRAR</button>
+                <div className="d-flex justify-content-center">
+                  <button type="submit" className="btn btn-primary" disabled={!isValid} style={{ backgroundColor: '#19A800' }}>ENTRAR</button>
                 </div>
               </form>
             </div>
