05bbe78cf0a1039e0370a4fe4793912591b03d2c
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2023-09-15 18:32:34 -0400

    Consegui arrumar o botão de logout

--- sig-mt-protege/src/pages/painel-principal.jsx
+++ sig-mt-protege/src/pages/painel-principal.jsx
@@ -1,51 +1,89 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Logo from '../images/logo-definitiva-mt-protege.png';
-import IconeLogout from '../images/icone-logout.png';
-import IconePainelPrincipal from '../images/icone-painel-principal.png';
-import IconePolicia from '../images/icone-policia.png';
-import IconeOcorrencia from '../images/icone-ocorrencia.png';
-import IconeAjuda from '../images/icone-ajuda.png';
-import './styles/styles.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Logo from "../images/logo-definitiva-mt-protege.png";
+import IconeLogout from "../images/icone-logout.png";
+import IconePainelPrincipal from "../images/icone-painel-principal.png";
+import IconePolicia from "../images/icone-policia.png";
+import IconeOcorrencia from "../images/icone-ocorrencia.png";
+import IconeAjuda from "../images/icone-ajuda.png";
+import "./styles/styles.css";
 
 function PainelPrincipal() {
-    return (
+  const navigate = useNavigate();
+  
+  return (
     <div className="container-fluid">
       <div className="row">
         {/*MENU DE NAVEGAÇÃO */}
-        <nav className="custom-bg-color" style={{  width: '18%', height: '100vh', position: 'relative' }}>
+        <nav
+          className="custom-bg-color"
+          style={{ width: "18%", height: "100vh", position: "relative" }}
+        >
           <div className="logo-container">
             <img src={Logo} alt="Minha Logo" className="logo" />
           </div>
           <ul className="nav flex-column">
             <li className="nav-item">
               <a className="nav-link text-light" href="/painel-principal">
-                <img src={IconePainelPrincipal} alt="Icone Painel Principal" className="icones-menu-nav" />PAINEL PRINCIPAL
+                <img
+                  src={IconePainelPrincipal}
+                  alt="Icone Painel Principal"
+                  className="icones-menu-nav"
+                />
+                PAINEL PRINCIPAL
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link text-light" href="/policiais">
-              <img src={IconePolicia} alt="Icone Policial" className="icones-menu-nav" />POLICIAIS
+                <img
+                  src={IconePolicia}
+                  alt="Icone Policial"
+                  className="icones-menu-nav"
+                />
+                POLICIAIS
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link text-light" href="/ocorrencias">
-                <img src={IconeOcorrencia} alt="Icone Ocorrencia" className="icones-menu-nav" />OCORRÊNCIAS
+                <img
+                  src={IconeOcorrencia}
+                  alt="Icone Ocorrencia"
+                  className="icones-menu-nav"
+                />
+                OCORRÊNCIAS
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link text-light" href="/ajuda">
-                <img src={IconeAjuda} alt="Icone Ajuda" className="icones-menu-nav" />AJUDA / SUPORTE
+                <img
+                  src={IconeAjuda}
+                  alt="Icone Ajuda"
+                  className="icones-menu-nav"
+                />
+                AJUDA / SUPORTE
               </a>
             </li>
           </ul>
 
-         {/* SAIR DO SISTEMA com ícone */}
-         <div style={{ position: 'absolute', bottom: '0', width: '90%' }}>
+          {/* SAIR DO SISTEMA com ícone */}
+          <div style={{ position: "absolute", bottom: "0", width: "90%" }}>
             <ul className="nav flex-column">
               <li className="nav-item">
-                <a className="nav-link text-light" href="/">
-                  <img src={IconeLogout} alt="Icone Logout" className="icone-logout" />SAIR DO SISTEMA
+                <a
+                  className="nav-link text-light"
+                  onClick={() => {
+                    sessionStorage.removeItem("token");
+                    navigate("/");
+                  }}
+                >
+                  <img
+                    src={IconeLogout}
+                    alt="Icone Logout"
+                    className="icone-logout"
+                  />
+                  SAIR DO SISTEMA
                 </a>
               </li>
             </ul>
@@ -59,6 +97,6 @@ function PainelPrincipal() {
       </div>
     </div>
   );
-  }
-  
+}
+
 export default PainelPrincipal;
